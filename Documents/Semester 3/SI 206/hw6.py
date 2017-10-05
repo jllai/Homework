10cd5923095de078ba4943a46cbc03fafc339bfa
Jamie Lai <jllai@umich.edu>
2017-10-05 09:11:39 -0400

    hw5.py

---
+++ Documents/Semester 3/SI 206/hw6.py
@@ -0,0 +1,31 @@
+import operator
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+
+# html.parser is the HTML parser included in the standard Python 3 library.
+# information on other HTML parsers is here:
+# http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+tags = soup('span')
+
+#list for the sum
+sumlst = []
+
+for tag in tags:
+    # Look at the parts of a tag
+    sumlst.append(int(tag.contents[0]))
+
+print(sum(sumlst))
